13851cce1816093c1a86cc9e2ffe536b2c2570ca
shihaobo <1282691999@qq.com>
2020-03-16 12:43:37 +0800

    。。。

--- src/index.js
+++ src/index.js
@@ -1,45 +1,5 @@
 import './static/css/style.css'
+// import './notice/point'
+import './notice/test'
 import util from './tool/util'
 
-/**
- * 1
- */
-const res1 = [10, 20, 10].map(parseInt)
-console.log(res1) //[10, NaN, 2]
-
-/**
- * 2、手写字符串trim
- */
-String.prototype.trim = function () {
-    return this.replace(/^\s+/,'').replace(/\s+$/,'')
-}
-
-/**
- * 手写bind函数
- */
-Function.prototype.bind1 = function(){
-    // 将参数拆分为数组
-    const args = Array.prototype.slice.call(arguments)
-
-    // 获取 this 数组第一项
-    const t = args.shift()
-    console.log(t,'ttt')
-
-    // f1.bind(...)中的f1
-    const self = this;
-    // 返回一个函数
-    return function(){
-        return self.apply(t,args)
-    }
-}
-
-function fn1(a, b, c){
-    console.log('this', this)
-    console.log(a, b, c)
-    return 'this is fn1'
-}
-const fn2 = fn1.bind1({x :100}, 10 ,20, 30)
-let res2 = fn2()
-
-var ipt1 = document.querySelectorAll('input')[0]
-var div1 = document.querySelectorAll('#div1')[0]
